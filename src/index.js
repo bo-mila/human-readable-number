8d172cc382ed639640a55f544ce6f5324d87a55c
Mila Bozhevskaya <mi.bozhev@gmail.com>
2022-01-08 01:28:36 +0200

    complete task readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+const numberNames = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+];
+const dozenNames = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+];
+
 module.exports = function toReadable (number) {
-  
-}
+      if (number < 20) {
+           return numberNames[number];
+      }
+      const i = number < 100 ? 10 : 100;
+      return ((i === 100) ? (numberNames[Math.floor(number / i)] + ' hundred') : dozenNames[Math.floor(number / i) - 2]) + ((number % i) ? (' ' + toReadable(number - Math.floor(number / i) * i)) : '');
+};
